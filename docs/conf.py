724533d3903e5a5f7eae6f52679a9602398c7fe8
Vincent (Leirof) <vince.lrf@gmail.com>
2023-03-11 19:06:46 +0100

    📝 Added linkify plugin

--- docs/conf.py
+++ docs/conf.py
@@ -97,7 +97,7 @@ myst_enable_extensions = [
     "fieldlist",
     "html_admonition",
     "html_image",
-    #"linkify",
+    "linkify",
     "replacements",
     "smartquotes",
     "strikethrough",
