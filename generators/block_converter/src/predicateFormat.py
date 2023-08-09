17ac9addeeabc6c602522bc5c6cb08485b22088f
Vincent (Leirof) <vince.lrf@gmail.com>
2023-08-09 14:19:33 +0200

    ♻️ Block2ID generator V2

--- generators/block_converter/src/predicateFormat.py
+++
@@ -1,5 +0,0 @@
-def getBlock():
-    return  """{{"condition":"minecraft:location_check","predicate":{{"block":{{"blocks":["minecraft:{BlockName}"],"state": {{{BlockStates}}}}}}}}}"""
-
-def getItem():
-    return """{{"condition":"minecraft:alternative","terms":[{{"condition":"minecraft:entity_properties","entity":"this","predicate":{{"type":"minecraft:item","nbt":"{{Item:{{id:\\\"minecraft:{BlockName}\\\"}}}}"}}}}]}}"""
