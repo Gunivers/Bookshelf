2681e6856dd9b67f7aa08dfa76d8720feef374e7
Aksiome <54895777+aksiome@users.noreply.github.com>
2024-03-10 23:48:34 +0100

    🐛 Fix giscus theme switch

--- docs/_static/bookshelf.js
+++ docs/_static/bookshelf.js
@@ -1,6 +1,6 @@
 const getTheme = () => {
   const theme = document.documentElement.dataset.theme;
-  return theme !== "light" && theme !== "dark" ? theme : (
+  return (theme === "light" || theme === "dark") ? theme : (
     window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
   )
 }
