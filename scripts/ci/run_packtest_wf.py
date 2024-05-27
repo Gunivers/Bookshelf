4da3864cf8374cf8d903de8a313832d30942684d
Aksiome <54895777+aksiome@users.noreply.github.com>
2024-05-27 22:30:10 +0200

    🐛 fix stdout buffer getting stucked

--- scripts/ci/run_packtest_wf.py
+++ scripts/ci/run_packtest_wf.py
@@ -1,8 +1,11 @@
 import definitions
+import re
+
 from packtest import Assets, Runner
 
 runner = Runner(Assets(definitions.MC_VERSION))
 with runner.run(definitions.DATAPACKS_PATH, definitions.ROOT_DIR) as process:
-    for line in iter(process.stdout.readline, b''):
-        print(line, end='')
+    for line in iter(process.stdout.readline, ""):
+        if match := re.search(r"::error title=(.*?)::(.*)", line):
+            print(line, end='')
     exit(process.wait())
