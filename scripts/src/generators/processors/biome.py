167f7f156f822ecbca804db4e9ed75bcddaae38a
Théo Giraudet <theo.giraudet@orange.fr>
2024-06-05 22:32:42 +0200

    📝 Update header generation

--- scripts/src/generators/processors/biome.py
+++ scripts/src/generators/processors/biome.py
@@ -1,6 +1,11 @@
+from datetime import datetime
 import math
 from collections import defaultdict
+import os
+from pathlib import Path
 from generators.contracts import DataProcessor
+from jinja2 import Environment, FileSystemLoader
+import definitions
 
 
 class CreateTagsFiles(DataProcessor):
@@ -45,7 +50,13 @@ class CreateTypesFile(DataProcessor):
     def process(self, data):
         print("\033[90m⚙️ Generating types table function\033[0m")
 
+        env = Environment(loader=FileSystemLoader(os.path.join(Path(definitions.ROOT_DIR) / "scripts" / "src" / "check_function_headers" / "templates")))
+        template = env.get_template("header.jinja")
+        header = template.render(year=datetime.now().year, doc=None, is_feature=False).replace("\r\n", "\n").strip()
+
         self.write_text(self.target, [
+            header,
+            "",
             "# This file was automatically generated, do not edit it",
             (f"data modify storage bs:const biome set value "
              f"{self.render(self.format(data))}")
