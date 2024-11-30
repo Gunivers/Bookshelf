6b0992b9f1552786d0565322881aec2a3a942ff7
Aksiome <54895777+aksiome@users.noreply.github.com>
2024-12-01 00:39:35 +0100

    Add auto batch to packtest

--- core/plugins/packtest.py
+++ core/plugins/packtest.py
@@ -3,9 +3,13 @@ from typing import ClassVar
 
 
 def beet_default(ctx: Context):
-    ctx.data.extend_namespace.append(TestFile)
+    ctx.data.extend_namespace.append(TestFunction)
+    yield
+    offset = len(ctx.template.render('core/header.jinja'))
+    for _, function in ctx.data.all(extend=TestFunction):
+        function.text = f'# @batch {ctx.data.name}\n' + function.text[offset:]
 
 
-class TestFile(TextFile):
+class TestFunction(TextFile):
     scope: ClassVar[NamespaceFileScope] = ("test",)
     extension: ClassVar[str] = ".mcfunction"
